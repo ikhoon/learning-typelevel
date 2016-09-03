d5d823b4ecd042a373d32637336062cdcacfeaf5
Ikhoon Eom <ih.pert@gmail.com>
2016-09-04 00:57:37 +0900

    TypeProjection

---
+++ src/test/scala/TypeProjectionSpec.scala
@@ -0,0 +1,17 @@
+import StSource.Aux
+import org.scalatest.{Matchers, WordSpec}
+
+/**
+  * Created by ikhoon on 2016. 9. 4..
+  */
+class TypeProjectionSpec extends WordSpec with Matchers {
+
+  "type projection" should {
+
+    "type parameter see existentially" in {
+      val ss = StSource(0) { i: Int => (i, i) }
+      assert(StSource.runStSource2(ss, ss.init) == (0, 0))
+
+    }
+  }
+}
